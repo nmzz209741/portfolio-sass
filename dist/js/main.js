e85d560eb38c6ed3880463c3681c6791b779cabc
shreeti <shreetis13@gmail.com>
2020-08-30 16:11:53 +0545

    menu nav

--- dist/js/main.js
+++ dist/js/main.js
@@ -1,5 +1,7 @@
 const menuBtn = document.querySelector('.menu-btn');
 const hamBurger = document.querySelector('.menu-btn__burger');
+const nav = document.querySelector('.nav');
+const menuNav = document.querySelector('.menu-nav');
 
 let showMenuBar = false;
 
@@ -8,10 +10,14 @@ menuBtn.addEventListener('click', toggleMenu);
 function toggleMenu() {
   if(!showMenuBar) {
     hamBurger.classList.add('open');
+    nav.classList.add('open');
+    menuNav.classList.add('open');
     showMenuBar = true;
 
   } else {
     hamBurger.classList.remove('open');
+    nav.classList.remove('open');
+    menuNav.classList.remove('open');
     showMenuBar = false;
   }
 }
